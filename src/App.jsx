a5a35156403209eb862d39b70c1795f942f4ba05
RohanShedage01 <shedagerohan21@gmail.com>
2026-04-26 21:04:37 +0530

    result page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/navbar';
 import Footer from './components/footer/footer';
 import Home from './views/home/home';
+<<<<<<< HEAD
 import Results from './views/results/results';
+=======
+import Aptitude from "./views/categories/aptitude.jsx";
+import dashboard from './views/dashboard/dashboard.jsx';
+>>>>>>> aa47276f522ece2aad77c2727c5f390a61eb6cb5
 
 function App() {
   return (
@@ -13,6 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/results" element={<Results />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
+          <Route path="/aptitude" element={<Aptitude />} />
+          <Route path="/dashboard" element={<Dashboard />}/>
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
