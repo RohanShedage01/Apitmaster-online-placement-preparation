30ae6478c687c88a78ecf6a79c9f2cdd15de9774
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-26 22:36:00 +0530

    create left side

--- src/views/signup/signup.jsx
+++ src/views/signup/signup.jsx
@@ -7,3 +7,30 @@ export default function Signup() {
     email: "",
     password: "",
   });
+
+  
+  return (
+    <div className="wrapper">
+      <div className="container">
+
+        {/* LEFT SIDE IMAGE */}
+        <div className="right">
+          <div className="right-text">
+            <h2>Join Us!</h2>
+            <p>Create your account and start your journey</p>
+
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+              alt="signup"
+              style={{ width: "120px", marginTop: "20px" }}
+            />
+          </div>
+        </div>
+
+        
+        </div>
+
+      </div>
+    </div>
+  );
+}
