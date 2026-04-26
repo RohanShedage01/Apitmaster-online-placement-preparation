72894cae6243e8e4014f6128a67f3d7eb59fefe9
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-04-26 22:34:16 +0530

    export signup function

--- src/views/signup/signup.jsx
+++ src/views/signup/signup.jsx
@@ -1,2 +1,9 @@
 import { useState } from "react";
 import "./login.css";// reuse same CSS
+
+export default function Signup() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
